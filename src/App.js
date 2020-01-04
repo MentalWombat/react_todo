994db4a9bc7e49bfb039be4242b2ce7181bef1f8
MentalWombat <e.ochneva@gmail.com>
2020-01-04 15:07:49 +0300

    add prop-types and eslint rules

--- src/App.js
+++ src/App.js
@@ -2,40 +2,12 @@ import React from 'react';
 import { Grommet } from 'grommet';
 import theme from './theme';
 import Board from './Board';
+import data from './data.json';
 
-const data = {
-  todo: [
-    {
-      id: 3,
-      text: 'Learn React'
-    },
-    {
-      id: 4,
-      text: 'Paint walls'
-    },
-    {
-      id: 5,
-      text: 'Read a book'
-    }
-  ],
-  done: [
-    {
-      id: 1,
-      text: 'Walk the dog'
-    },
-    {
-      id: 2,
-      text: 'Buy a book'
-    }
-  ]
-};
-
-const App = () => {
-  return (
-    <Grommet theme={theme}>
-      <Board data={data} />
-    </Grommet>
-  );
-}
+const App = () => (
+  <Grommet theme={theme}>
+    <Board data={data} />
+  </Grommet>
+);
 
 export default App;
--- src/Board.js
+++ src/Board.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Box, Heading } from 'grommet';
 import board from './board.jpg';
 
 const Item = ({ text, color }) => (
   <Box
+    align="center"
     background={color}
-    elevation='large'
-    margin='medium'
-    pad='medium'
-    height='small'
-    width='small'
+    elevation="large"
+    justify="center"
+    margin="medium"
+    pad="medium"
+    height="small"
+    width="small"
   >
     {text}
   </Box>
@@ -21,7 +24,7 @@ const ListItems = ({ items, color }) => (
 
 const List = ({ items, color, title }) => (
   <Box>
-    <Heading color={color} textAlign='center'>{title}</Heading>
+    <Heading color={color} textAlign="center">{title}</Heading>
     <ListItems items={items} color={color} />
   </Box>
 );
@@ -35,13 +38,33 @@ const Board = ({ data }) => (
       repeat: 'repeat',
       size: 'auto'
     }}
-    direction='row'
-    height='100vh'
-    pad='large'
+    direction="row"
+    height="100vh"
+    pad="large"
   >
-    <List items={data.todo} color='neutral-2' title='To Do' />
-    <List items={data.done} color='neutral-3' title='Done' />
+    <List items={data.todo} color="neutral-2" title="To Do" />
+    <List items={data.done} color="neutral-3" title="Done" />
   </Box>
 );
+
+Board.propTypes = {
+  data: PropTypes.objectOf(PropTypes.array).isRequired
+};
+
+List.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.object).isRequired,
+  color: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired
+};
+
+ListItems.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.object).isRequired,
+  color: PropTypes.string.isRequired
+};
+
+Item.propTypes = {
+  text: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired
+};
 
 export default Board;
